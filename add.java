3cb2e47effbfdcbfd9fece3363b2527d9f52cf3b
Dharshini N <dharshini20050916@gmail.com>
2026-02-05 13:34:29 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,15 @@
+package Student.java;
+
+public class add {
+
+    public static void main(String[] args) {
+        System.out.println(add());
+    }
+
+    public static int add() {
+        int a = 20;
+        return a;
+    }
+}
+
+ 

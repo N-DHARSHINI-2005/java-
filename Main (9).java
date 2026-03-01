7f59a6fb3fde54a4c1a60250062189ecf9e7ee2d
Dharshini N <dharshini20050916@gmail.com>
2026-03-01 23:11:34 +0530

    Add files via upload

---
+++ Main (9).java
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    int marks = 38;
+int grace = 2;
+
+if (marks >= 40 || (marks + grace) >= 40) {
+    System.out.println("Pass");
+} else {
+    System.out.println("Fail");
+}
+	
+	}
+}
